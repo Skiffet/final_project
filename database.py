26201c1cca45f682b9fbdf98d6a0d28e2b734c39
Thanutham <thanutham.c@ku.th>
2023-11-27 18:00:24 +0700

    add 2 classes in this file

--- database.py
+++ database.py
@@ -30,7 +30,65 @@ class Database:
 
     def find_table(self, table_name):
         for table in self.database:
-            if table.name == table_name:
+            if table.table_name == table_name:
                 return table
         return None
+
+import copy
+
+
+class Table:
+    def __init__(self, table_name, rows):
+        self.table_name = table_name
+        self.rows = [Row(row_data) for row_data in rows]
+
+    def join(self, other_table, common_key):
+        joined_table = Table(
+            f"{self.table_name}_joins_{other_table.table_name}", [])
+        for row1 in self.rows:
+            for row2 in other_table.rows:
+                if getattr(row1, common_key) == getattr(row2, common_key):
+                    joined_row = Row({**row1.data, **row2.data})
+                    joined_table.rows.append(joined_row)
+        return joined_table
+
+    def filter(self, condition):
+        filtered_table = Table(f"{self.table_name}_filtered", [])
+        for row in self.rows:
+            if condition(row.data):
+                filtered_table.rows.append(row)
+        return filtered_table
+
+    def aggregate(self, function, aggregation_key):
+        temps = [float(getattr(row, aggregation_key)) for row in self.rows]
+        return function(temps)
+
+    def select(self, attributes_list):
+        selected_rows = [Row({key: getattr(row, key) for key in attributes_list}) for row in self.rows]
+        return selected_rows
+
+    def insert(self, data):
+        new_row = Row(data)
+        self.rows.append(new_row)
+
+    def update(self, row_id, column, value):
+        for row in self.rows:
+            if getattr(row, 'ID') == row_id:
+                setattr(row, column, value)
+
+    def __str__(self):
+        return f"{self.table_name}:{[str(row) for row in self.rows]}"
+
+
+class Row:
+    def __init__(self, data):
+        self.data = data
+
+    def __str__(self):
+        return str(self.data)
+
+
+
+
+
 
